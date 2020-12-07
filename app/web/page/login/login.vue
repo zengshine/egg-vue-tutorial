69dcd0389ce09f13e1bde5231355499e04576535
yaohui.zeng <yaohui.zeng@qizhidao.com>
2020-12-07 13:45:41 +0800

    docs: 删除无用文件

--- app/web/page/login/login.vue
+++ app/web/page/login/login.vue
@@ -1,53 +1,51 @@
 <template>
-  <Layout>
-    <div class="login">
-      <div class="login-form">
-        <div class="login-header">
-          <img
-            src=""
-            height="100"
-            alt=""
-          >
-          <p>easyjs</p>
-        </div>
-        <div class="login-info">
-          <input
-            v-model="username"
-            placeholder="请输入用户名"
-            suffix-icon="fa fa-user"
-            style="margin-bottom: 18px"
-          >
-
-          <input
-            v-model="password"
-            placeholder="请输入密码"
-            suffix-icon="fa fa-keyboard-o"
-            type="password"
-            style="margin-bottom: 18px"
-          >
-        </div>
-
-        <button
-          type="button"
-          style="width: 100%;margin-bottom: 18px"
-          onclick="javascript: location.href='/admin/'"
+  <div class="login">
+    <div class="login-form">
+      <div class="login-header">
+        <img
+          src=""
+          height="100"
+          alt=""
         >
-          登录
-        </button>
-        <div>
-          <input
-            v-model="remenber"
-            type="checkbox"
-            class="login-remember"
-          >记住密码
-          <a
-            href="/admin/"
-            style="float: right;color: #3C8DBC;font-size: 14px"
-          >注册</a>
-        </div>
+        <p>easyjs</p>
+      </div>
+      <div class="login-info">
+        <input
+          v-model="username"
+          placeholder="请输入用户名"
+          suffix-icon="fa fa-user"
+          style="margin-bottom: 18px"
+        >
+
+        <input
+          v-model="password"
+          placeholder="请输入密码"
+          suffix-icon="fa fa-keyboard-o"
+          type="password"
+          style="margin-bottom: 18px"
+        >
+      </div>
+
+      <button
+        type="button"
+        style="width: 100%;margin-bottom: 18px"
+        onclick="javascript: location.href='/admin/'"
+      >
+        登录
+      </button>
+      <div>
+        <input
+          v-model="remenber"
+          type="checkbox"
+          class="login-remember"
+        >记住密码
+        <a
+          href="/admin/"
+          style="float: right;color: #3C8DBC;font-size: 14px"
+        >注册</a>
       </div>
     </div>
-  </Layout>
+  </div>
 </template>
 
 <script lang="ts">
