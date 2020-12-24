851a25157fe7ed62bcf0213dba588833c62af59b
yaohui.zeng <yaohui.zeng@qizhidao.com>
2020-12-24 09:51:28 +0800

    build: 更新package.json脚本

--- typings/app/model/index.d.ts
+++ typings/app/model/index.d.ts
@@ -2,13 +2,11 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExportIndex = require('../../../app/model/index');
 import ExportUser from '../../../app/model/user';
 import ExportTestTest from '../../../app/model/test/test';
 
 declare module 'egg' {
   interface IModel {
-    Index: ReturnType<typeof ExportIndex>;
     User: ReturnType<typeof ExportUser>;
     Test: {
       Test: ReturnType<typeof ExportTestTest>;
