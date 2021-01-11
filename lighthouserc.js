77cf8703b8b72ae45f0eee08244d20d09cc4c0a1
yaohui.zeng <yaohui.zeng@qizhidao.com>
2021-01-11 21:21:09 +0800

    ci: 更新Dockerfile

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,9 +4,13 @@ module.exports = {
       // 静态资源目录
       staticDistDir: './public',
       startServerCommand: 'npm run start',
+      startServerReadyPattern: 'egg started on http://127.0.0.1:7001',
       url: [ 'http://localhost:7001' ],
       // 测量过程运行次数
-      numberOfRuns: 2
+      numberOfRuns: 2,
+      settings: {
+        chromeFlags: '--no-sandbox'
+      }
     },
 
     assert: {
