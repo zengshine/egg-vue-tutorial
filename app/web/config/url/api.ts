24e5cba946f4d6113a21417874ca667d2e222fb6
= <=>
2021-01-20 00:50:14 +0800

    fix: 更新api url前缀

--- app/web/config/url/api.ts
+++ app/web/config/url/api.ts
@@ -1,7 +1,8 @@
 /**
  * @file API基础url
  */
-const PROD_ENV_BASIC_URL = 'http://localhost:40001/mock';
+// const PROD_ENV_BASIC_URL = 'http://localhost:40001/mock';
+const PROD_ENV_BASIC_URL = '';
 const DEV_ENV_BASIC_URL = '';
 
 const API_BASIC_URL = process.env.NODE_ENV === 'production' ? PROD_ENV_BASIC_URL : DEV_ENV_BASIC_URL;
