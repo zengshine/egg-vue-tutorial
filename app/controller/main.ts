01ac0269f789671b64b426f155db7f80f70cee34
yaohui.zeng <yaohui.zeng@qizhidao.com>
2021-01-20 11:31:12 +0800

    ci: 更新nginx配置

--- app/controller/main.ts
+++ app/controller/main.ts
@@ -15,15 +15,15 @@ export default class HomeController extends Controller {
   public async server() {
     const { ctx } = this;
 
-    const url = ctx.request.url;
     // main/index.js 对应 webpack entry 的 main/index, 构建后文件存在 app/view 目录
-    await ctx.render('main/index.js', { message: 'egg vue server side render', url });
+    await ctx.render('main/index.js', { message: 'egg vue server side render' });
   }
 
   async client() {
     const { ctx } = this;
+    const url = ctx.request.url;
     // renderClient 前端渲染，Node层只做 layout.html和资源依赖组装，渲染交给前端渲染。
     // 与服务端渲染的差别你可以通过查看运行后页面源代码即可明白两者之间的差异
-    await ctx.renderClient('main/index.js', { message: 'egg vue client render render' });
+    await ctx.renderClient('main/index.js', { message: 'egg vue client render render', url });
   }
 }
